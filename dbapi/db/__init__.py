905aa502bf06a1235add7ecafa26a9d2454ff911
longfengpili <398745129@qq.com>
2020-06-08 13:20:07 +0800

    [新增]支持sqlite

--- dbapi/db/__init__.py
+++ dbapi/db/__init__.py
@@ -1,12 +1,13 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-03 10:31:52
-# @Last Modified time: 2020-06-03 18:15:49
+# @Last Modified time: 2020-06-08 12:26:22
 # @github: https://github.com/longfengpili
 
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 from .redshift import RedshiftDB
+from .sqlite import SqliteDB
 
-__all__ = ['RedshiftDB']
+__all__ = ['RedshiftDB', 'SqliteDB']
