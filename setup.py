6429febbdfb48a08740e28c355598813d81963f7
longfengpili <398745129@qq.com>
2020-06-10 17:57:11 +0800

    [优化]setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-09 16:46:54
-# @Last Modified time: 2020-06-10 17:43:55
+# @Last Modified time: 2020-06-10 17:53:15
 # @github: https://github.com/longfengpili
 
 #!/usr/bin/env python3
@@ -12,7 +12,8 @@ import sys
 import shutil
 import setuptools
 
-VERSION = '0.0.9'
+VERSION = '0.0.10'
+PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
@@ -24,31 +25,32 @@ if sys.argv[1] == 'build':
     os.system('python setup.py sdist bdist_wheel')
 elif sys.argv[1] == 'publish':
     os.system('python setup.py sdist bdist_wheel')
-    os.system(f'python -m twine upload dist/*{VERSION}* --repository pydbapi')
+    os.system(f'python -m twine upload dist/*{VERSION}* --repository {PROJECT_NAME}')
+    shutil.rmtree(f'./{PROJECT_NAME}-{VERSION}')
     shutil.rmtree(f'./build')
-    shutil.rmtree(f'./pydbapi.egg-info')
+    shutil.rmtree(f'./{PROJECT_NAME}.egg-info')
     sys.exit()
 
 setuptools.setup(
-    name="pydbapi", # Replace with your own username
-    version=VERSION,
-    author="longfengpili",
-    author_email="398745129@qq.com",
-    description="A simple database API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://pypi.org/project/pydbapi/",
-    packages=setuptools.find_packages(),
-    install_requires=requires,
-    classifiers=[
+    name = PROJECT_NAME, # Replace with your own username
+    version = VERSION,
+    author = "longfengpili",
+    author_email = "398745129@qq.com",
+    description = "A simple database API",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = f"https://pypi.org/project/{PROJECT_NAME}/",
+    packages = setuptools.find_packages(),
+    install_requires = requires,
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
     keywords = ["dbapi", "sqlite3", "redshift"], 
-    python_requires='>=3.6',
-    project_urls={
-        'Documentation': 'https://github.com/longfengpili/pydbapi/blob/master/README.md',
-        'Source': 'https://github.com/longfengpili/pydbapi',
+    python_requires = '>=3.6',
+    project_urls = {
+        'Documentation': f'https://github.com/longfengpili/{PROJECT_NAME}/blob/master/README.md',
+        'Source': f'https://github.com/longfengpili/{PROJECT_NAME}',
     },
 )
 
