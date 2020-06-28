591b2c064c399ad657298fea889c0e04785185ef
longfengpili <398745129@qq.com>
2020-06-28 12:13:26 +0800

    [新增]file argument 和 func argument

--- pydbapi/sql/parse.py
+++ pydbapi/sql/parse.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-03 10:51:08
-# @Last Modified time: 2020-06-17 21:08:18
+# @Last Modified time: 2020-06-28 12:05:43
 # @github: https://github.com/longfengpili
 
 #!/usr/bin/env python3
@@ -9,6 +9,12 @@
 
 import re
 import os
+
+import logging
+import logging.config
+from pydbapi.conf import LOGGING_CONFIG
+logging.config.dictConfig(LOGGING_CONFIG)
+sqllogger = logging.getLogger('sql')
 
 class SqlParse(object):
 
@@ -59,24 +65,60 @@ class SqlFileParse(object):
         return content
 
     @property
-    def params(self):
+    def arguments(self):
+        '''[summary]
+        
+        [description]
+            获取文件中配置的arguments
+        Returns:
+            [dict] -- [返回文件中的参数设置]
+        '''
+        arguments = {}
         content = self.get_content()
         content = re.sub('--.*?\n', '\n', content) #去掉注释
-        params = re.findall(rf"\$(\w+){self.reg_behind}", content)
-        return set(params)
+        arguments_temp = re.findall(r'(?<!--)\s+#【argument】#\n(.*?)#【argument】#', content, re.S)
+        arguments_temp = ';'.join(arguments_temp).replace('\n', ';')
+        arguments_temp = [argument.strip() for argument in arguments_temp.split(';') if argument]
+        for argument in arguments_temp:
+            arg, value = argument.split('=', 1)
+            arguments[arg.strip()] = value.strip()
+        return arguments
+
+    @property
+    def parameters(self):
+        content = self.get_content()
+        content = re.sub('--.*?\n', '\n', content) #去掉注释
+        parameters = re.findall(rf"\$(\w+){self.reg_behind}", content)
+        return set(parameters)
 
     def replace_params(self, **kw):
-        params_diff = self.params - set(kw)
+        '''[summary]
+        
+        [description]
+            替换具体的参数值，传入的参数值会覆盖文件中设置的参数值
+        Arguments:
+            **kw {[参数]} -- [传入的参数值]
+        
+        Returns:
+            [str] -- [替换过后的内容]
+        
+        Raises:
+            Exception -- [需要设置参数]
+        '''
+        arguments = self.arguments
+        arguments_same = set(arguments) & set(kw)
+        if arguments_same:
+            sqllogger.warning(f"{arguments_same} will use the func input arguments, not use sqlfile setting !")
+
+        arguments.update(kw)
+        params_diff = self.parameters - set(arguments)
         if params_diff:
             raise Exception(f"Need params 【{'】, 【'.join(params_diff)}】 !")
 
         content = self.get_content()
-        for key, value in kw.items():
-            if re.search(rf'(?<!in )(\${key})', content): # 检查是否有非in的情况
-                content = re.sub(rf"(?<!in )\${key}{self.reg_behind}", f"'{value}'", content)
-            else:
-                value = "('" + "', '".join([v for v in value.split(',')]) + "')"
-                content = re.sub(rf"(?<=in )\${key}{self.reg_behind}", f"{value}", content)
+        for key, value in arguments.items():
+            content = re.sub(rf"\${key}{self.reg_behind}", f"{value}", content)
+        sqllogger.warning(f"The file {os.path.basename(self.filepath)} use arguments {arguments}")
         return content
 
     def get_sqls(self, **kw):
@@ -84,11 +126,9 @@ class SqlFileParse(object):
         content = self.replace_params(**kw)
         sqls_tmp = re.findall(r'(?<!--)\s+###\n(.*?)###', content, re.S)
         for idx, sql in enumerate(sqls_tmp):
-            purpose = re.match('--(【.*?)\n', sql.strip())
-            purpose = purpose.group(1) if purpose else f'No description {idx}'
+            purpose = re.match('--(【.*?】)\n', sql.strip())
+            purpose = f"{purpose.group(1)}--第{idx+1}个SQL" if purpose else f'【No description】--第{idx+1}个SQL'
             sql = re.sub('--【.*?\n', '', sql.strip())
             sqls[purpose] = sql
         return sqls
-
-
 
