6f798a8e1502052863bd189a73d8d3e6ae2f844a
longfengpili <398745129@qq.com>
2020-07-01 16:47:12 +0800

    [测试]测试workflow

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# @Author: chunyang.xu
+# @Email:  398745129@qq.com
+# @Date:   2020-06-11 12:04:43
+# @Last Modified time: 2020-06-11 12:05:04
+# @github: https://github.com/longfengpili
+
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
