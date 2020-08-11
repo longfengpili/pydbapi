153a0801e56f0d2ef5f45cc7f4b9aa1a0264352f
longfengpili <398745129@qq.com>
2020-08-11 19:18:47 +0800

    [优化]verbose

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-09 16:46:54
-# @Last Modified time: 2020-08-11 14:36:31
+# @Last Modified time: 2020-08-11 19:18:42
 # @github: https://github.com/longfengpili
 
 #!/usr/bin/env python3
@@ -12,7 +12,7 @@ import sys
 import shutil
 import setuptools
 
-VERSION = '0.0.29'
+VERSION = '0.0.30'
 PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
