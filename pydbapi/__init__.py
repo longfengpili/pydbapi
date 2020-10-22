3c6ce997fef5329637bba7610dd5cb808d1002dc
longfengpili <398745129@qq.com>
2020-10-22 16:36:59 +0800

    [新增]snowflake

--- pydbapi/__init__.py
+++ pydbapi/__init__.py
@@ -1,12 +1,12 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-04 10:58:54
-# @Last Modified time: 2020-06-17 21:08:15
+# @Last Modified time: 2020-10-22 16:34:03
 # @github: https://github.com/longfengpili
 
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from pydbapi.api import SqliteDB, RedshiftDB, MysqlDB
+from pydbapi.api import SqliteDB, RedshiftDB, MysqlDB, SnowflakeDB
 
-__all__ = ['SqliteDB', 'RedshiftDB', 'MysqlDB']
+__all__ = ['SqliteDB', 'RedshiftDB', 'MysqlDB', 'SnowflakeDB']
--- pydbapi/api/__init__.py
+++ pydbapi/api/__init__.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-08 14:02:33
-# @Last Modified time: 2020-06-17 21:08:54
+# @Last Modified time: 2020-10-22 16:24:00
 # @github: https://github.com/longfengpili
 
 #!/usr/bin/env python3
@@ -10,6 +10,7 @@
 from .redshift import RedshiftDB
 from .sqlite import SqliteDB
 from .mysql import MysqlDB
+from .snowflake import SnowflakeDB
 
 __doc__ = "数据库接口"
-__all__ = ['RedshiftDB', 'SqliteDB', 'MysqlDB']
+__all__ = ['RedshiftDB', 'SqliteDB', 'MysqlDB', 'SnowflakeDB']
