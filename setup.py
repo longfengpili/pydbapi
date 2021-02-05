858a2f0078ab9fabbd64fb73c8940b7be253aae8
longfengpili <398745129@qq.com>
2021-02-05 14:23:57 +0800

    [修改]psycopg2-binary

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-09 16:46:54
-# @Last Modified time: 2021-02-02 12:28:46
+# @Last Modified time: 2021-02-05 14:21:10
 # @github: https://github.com/longfengpili
 
 # !/usr/bin/env python3
@@ -12,7 +12,7 @@ import sys
 import shutil
 import setuptools
 
-VERSION = '0.0.36'
+VERSION = '0.0.37'
 PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
