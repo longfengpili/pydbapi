c6fa8193fff42939f51f1fd6c383e2deec9d355c
longfengpili <398745129@qq.com>
2021-04-19 20:12:42 +0800

    [优化]parse

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-09 16:46:54
-# @Last Modified time: 2021-04-19 19:53:30
+# @Last Modified time: 2021-04-19 20:12:38
 # @github: https://github.com/longfengpili
 
 # !/usr/bin/env python3
@@ -12,7 +12,7 @@ import sys
 import shutil
 import setuptools
 
-VERSION = '0.0.55'
+VERSION = '0.0.56'
 PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
