e05865be42531db9fe47c3dd5332fe9992996287
longfengpili <398745129@qq.com>
2021-11-20 14:58:53 +0800

    [修复]单例模式

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-09 16:46:54
-# @Last Modified time: 2021-11-20 10:22:31
+# @Last Modified time: 2021-11-20 14:58:40
 # @github: https://github.com/longfengpili
 
 # !/usr/bin/env python3
@@ -12,7 +12,7 @@ import sys
 import shutil
 import setuptools
 
-VERSION = '0.0.79'
+VERSION = '0.0.80'
 PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
