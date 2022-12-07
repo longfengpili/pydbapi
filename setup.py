1e0d3de54f394b8ab979475510de705689b904e2
longfengpili <398745129@qq.com>
2022-12-07 17:32:02 +0800

    [修复]split_withsqls

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2020-06-09 16:46:54
-# @Last Modified time: 2022-11-24 17:18:18
+# @Last Modified time: 2022-12-07 17:31:43
 # @github: https://github.com/longfengpili
 
 # !/usr/bin/env python3
@@ -12,7 +12,7 @@ import sys
 import shutil
 import setuptools
 
-VERSION = '0.0.104'
+VERSION = '0.0.105'
 PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
