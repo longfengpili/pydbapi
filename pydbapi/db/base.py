696db89b38e8892ed93b12b4c04068047515307a
longfengpili <398745129@qq.com>
2023-11-29 14:07:14 +0800

    [优化]log

--- pydbapi/db/base.py
+++ pydbapi/db/base.py
@@ -2,7 +2,7 @@
 # @Author: longfengpili
 # @Date:   2023-06-02 15:27:41
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2023-11-14 18:37:59
+# @Last Modified time: 2023-11-29 13:58:55
 # @github: https://github.com/longfengpili
 
 
--- pydbapi/db/fileexec.py
+++ pydbapi/db/fileexec.py
@@ -2,7 +2,7 @@
 # @Author: longfengpili
 # @Date:   2023-06-02 15:27:41
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2023-07-27 15:34:45
+# @Last Modified time: 2023-11-29 14:01:56
 # @github: https://github.com/longfengpili
 
 
