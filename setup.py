429724cb299dfe9e2ea076496388049b6f328509
longfengpili <398745129@qq.com>
2024-03-01 10:17:43 +0800

    [新增]trino alter tablename

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 # @Author: longfengpili
 # @Date:   2023-06-02 15:27:41
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2024-02-29 18:41:27
+# @Last Modified time: 2024-03-01 10:17:37
 # @github: https://github.com/longfengpili
 
 
 import setuptools
 
-VERSION = '0.0.118'
+VERSION = '0.0.119'
 PROJECT_NAME = 'pydbapi'
 
 with open('README.md', 'r', encoding='utf-8') as f:
