ddaa889443e7682bd4b17ed5195740df9f6b8649
longfengpili <398745129@qq.com>
2024-11-20 10:49:34 +0800

    [优化]sqlparse

--- pydbapi/sql/parse.py
+++ pydbapi/sql/parse.py
@@ -2,14 +2,14 @@
 # @Author: longfengpili
 # @Date:   2024-10-09 16:33:05
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2024-10-12 14:18:47
+# @Last Modified time: 2024-11-20 10:46:09
 # @github: https://github.com/longfengpili
 
 
 import re
 
 import sqlparse
-from sqlparse.sql import Statement, Token, TokenList, Identifier, IdentifierList, Comment
+from sqlparse.sql import Token, TokenList, Identifier, IdentifierList, Comment
 from sqlparse.tokens import DML, DDL, CTE
 from sqlparse.tokens import Keyword, Newline, Punctuation
 
@@ -17,161 +17,74 @@ import logging
 sqllogger = logging.getLogger(__name__)
 
 
-class SqlParse:
+class SqlStatement:
 
-    def __init__(self, orisql: str):
-        self.orisql = orisql
-        self.verbose = 1
+    def __init__(self, sql: str):
+        self._sql = sql
+        self._parsed = sqlparse.parse(self._sql)[0]
+
+    def __repr__(self):
+        _repr = f"[{self.action}]{self.tablename}"
+        _repr = f"{_repr}::{self.comment}" if self.comment else _repr
+        return _repr
+
+    @classmethod
+    def from_sqlsnippets(cls, *sqlsnippets: tuple[str, ]) -> 'SqlStatement':
+        sql = '\n'.join(sqlsnippets)
+        return cls(sql)
 
     @property
-    def statements(self):
-        statements = sqlparse.parse(self.orisql)
-        if len(statements) > 1 and self.verbose:
-            self.verbose = 0
-            sqllogger.warning(f'SQL has {len(statements)} statements ~')
-        return statements
+    def tokens(self):
+        return self._parsed.tokens
 
     @property
     def sql(self):
-        sql = sqlparse.format(self.orisql, keyword_case='lower', strip_comments=True, use_space_around_operators=True)
-        return sql
-
-    def get_statement(self, idx: int = 0):
-        statements = self.statements
-        stmt = statements[idx]
-        return stmt
-
-    def get_first_comment(self):
-        first_stmt = self.get_statement()
-        for token in first_stmt.tokens:
-            if token.ttype == Newline:
-                continue
-            elif isinstance(token, Comment):
-                first_comment = token.value
-                return first_comment
-            else:
-                break
-        return '--No purpose--No comment'
-
-    @property
-    def purpose(self):
-        first_comment = self.get_first_comment()
-        purpose = re.search(r'--(.*?)--.*?$', first_comment, re.S)
-        if purpose:
-            purpose = purpose.group(1).strip()
-        return purpose
+        sql = sqlparse.format(self._sql, keyword_case='lower', strip_comments=True, use_space_around_operators=True)
+        return sql.strip()
 
     @property
     def comment(self):
-        first_comment = self.get_first_comment()
-        comment = re.search(r'(?:--.*?)?--(.*?)$', first_comment, re.S)
-        if comment:
-            comment = comment.group(1).strip()
-        return comment
+        for token in self.tokens:
+            if token.ttype == Newline:
+                continue
+            if isinstance(token, Comment):
+                comment = token.value
+                comment = re.search(r'^[-#]*(.*?)$', comment, re.S)
+                if comment:
+                    comment = comment.group(1).strip()
+                    comment = comment.replace('\n', ' ')
+                return comment
+        # return 'NoComment'
 
     @property
     def action(self):
-        first_stmt = self.get_statement()
-        for token in first_stmt.tokens:
+        for token in self.tokens:
             if token.ttype in (DML, DDL, CTE):
-                return token.value
-
-    def get_tables(self, tokenlist: [TokenList, Statement]):
-        tables = []
-        from_seen = True
-        # action = None
-        for token in tokenlist.tokens:
-            if from_seen and isinstance(token, Identifier):
-                tablename = token.value
-                if not re.match(r'([\w_]+\.)*\w+$', tablename):
-                    tablename = token.get_real_name()
-                # tablename = f'{tablename}({action})'
-                tables.append(tablename)
-            elif token.ttype in (DML, DDL, CTE):
-                # action = token.value
-                if token.value.lower() == 'select':
-                    from_seen = False
-            elif token.ttype is Keyword and token.value.lower() in ('from', 'join', 'into', 'update', 'delete'):
-                from_seen = True
-
-            # 暂时不考虑IdentifierList，后面再解决
-            # if isinstance(token, IdentifierList):
-            #     for identifier in token.get_identifiers():
-            #         tables.append(identifier.get_real_name())
-        return tables
+                return token.value.lower()
 
     @property
     def tablename(self):
-        first_stmt = self.get_statement()
-        tables = self.get_tables(first_stmt)
-        if tables:
-            return tables[0]
-
-    def get_subqueries(self, tokens: list, subtokens: list[Token] = None, subqueries: list[TokenList, ] = None, keep_last: bool = True):
-        def append_subquery(subtokens: list, subqueries: list):
-            _subqueries = subtokens.copy()
-            sub_tokenlist = TokenList(_subqueries)
-            subtoken_first = sub_tokenlist.token_first(skip_cm=True)
-            if subtoken_first:
-                subqueries.append(sub_tokenlist)
-            subtokens.clear()
-
-        if subqueries is None:
-            subqueries = []
-        if subtokens is None:
-            subtokens = []
-        islast = False
-
-        for token in tokens:
-            if isinstance(token, Comment):
-                continue
-            elif token.ttype == Newline and not subtokens:
-                continue
+        from_seen = True
+        for token in self.tokens:
+            if from_seen and isinstance(token, Identifier):
+                tablename = token.value
+                if ' ' in tablename:
+                    tablename = token.get_real_name()
+                return tablename
             elif token.ttype in (DML, DDL, CTE):
-                append_subquery(subtokens, subqueries)
-                subtokens.append(token)
                 if token.value.lower() == 'select':
-                    islast = True
-            elif token.ttype == Punctuation and not islast:
-                append_subquery(subtokens, subqueries)
-            elif isinstance(token, Identifier):  # Identifier 也是 TokenList, 所有必须在下个判断之前
-                subtokens.append(token)
-            elif isinstance(token, TokenList) or isinstance(token, IdentifierList):
-                self.get_subqueries(token.tokens, subtokens, subqueries)
+                    from_seen = False
+            elif token.ttype is Keyword and token.value.lower() in ('from', 'join', 'into'):
+                from_seen = True
             else:
-                subtokens.append(token)
-        if keep_last:
-            append_subquery(subtokens, subqueries)
-
-        return subqueries
+                pass
 
     @property
-    def combination_sqls(self):
-        combination_sqls = []
-        first_stmt = self.get_statement()
-        subqueries = self.get_subqueries(first_stmt.tokens, keep_last=False)
-        for idx, tokenlist in enumerate(subqueries):
-            tables = self.get_tables(tokenlist)
-            tablename = tables[0] if tables else ''
+    def params(self):
+        params = re.findall(r"\$(\w+)", self._sql)
+        return set(params)
 
-            # 生成注释内容和SELECT语句
-            content = f"-- {tablename}_{idx + 1:03d}"
-            sql_select = f'select * from {tablename} limit 10'
-
-            # 组合前面的SQL
-            combined_sql = ',\n'.join([subquery.value for subquery in subqueries[:idx+1]])
-            combined_sql = f'{content}\n{combined_sql}\n{sql_select}'
-
-            combination_sqls.append(combined_sql)
-        return combination_sqls
-
-    @property
-    def parameters(self):
-        sql = self.sql
-        parameters = re.findall(r"\$(\w+)", sql)
-        return set(parameters)
-
-    def substitute_parameters(self, **kwargs):
+    def substitute_params(self, **kwargs):
         '''[summary]
 
         [description]
@@ -183,13 +96,99 @@ class SqlParse:
             [str] -- [替换过后的内容]
 
         '''
-        params_diff = set(self.parameters) - set(kwargs)
+        params_diff = set(self.params) - set(kwargs)
         if params_diff:
             missing_params = ', '.join(params_diff)
-            raise Exception(f"Missing parameters: {missing_params}. Please provide values for all parameters.")
+            raise Exception(f"Missing params: {missing_params}. Please provide values for all params.")
 
-        sql = self.sql
         for key, value in kwargs.items():
-            sql = re.sub(rf"\${key}", f"{value}", sql)
+            self._sql = re.sub(rf"\${key}", f"{value}", self._sql)
 
-        return sql
+        return self
+
+    def get_subqueries(self, tokens: list[Token], 
+                       subtokens: list[Token] = None, 
+                       subqueries: list[TokenList, ] = None, 
+                       keep_last: bool = True) -> list[TokenList]:
+        def append_subquery(subtokens: list, subqueries: list):
+            _subqueries = subtokens.copy()  # subtokens会clear，所以需要复制到另外的变量
+            identifier = Identifier(_subqueries)
+            tokenfirst = identifier.token_first(skip_cm=True)
+            if tokenfirst:
+                subqueries.append(identifier)
+            subtokens.clear()
+
+        if subqueries is None:
+            subqueries = []
+        if subtokens is None:
+            subtokens = []
+        islast = False
+
+        for token in tokens:
+            # print(f"{type(token)}::{token.ttype}::{token.value}")  
+            if isinstance(token, Comment) or (token.ttype == Newline and not subtokens):
+                continue
+
+            if token.ttype in (DML, DDL, CTE):
+                append_subquery(subtokens, subqueries)
+                subtokens.append(token)
+                if token.value.lower() == 'select':
+                    islast = True
+            elif token.ttype == Punctuation and not islast:
+                append_subquery(subtokens, subqueries)
+            elif isinstance(token, Identifier):  # Identifier 也是 TokenList, 所有必须在下个判断之前
+                subtokens.append(token)
+            elif isinstance(token, (TokenList, IdentifierList)):
+                self.get_subqueries(token.tokens, subtokens, subqueries)
+            else:
+                subtokens.append(token)
+
+        if keep_last: 
+            append_subquery(subtokens, subqueries)
+
+        return subqueries
+
+    @property
+    def subqueries(self):
+        tokens = self.tokens
+        subqueries = self.get_subqueries(tokens, keep_last=False)
+        return subqueries
+
+    @property
+    def combination_sqls(self):
+        combination_sqls = []
+        subqueries = self.subqueries
+        for idx, identifier in enumerate(subqueries):
+            tablename = identifier.get_real_name()
+
+            # 生成注释内容和SELECT语句
+            comment = f"-- {tablename}_{idx + 1:03d}"
+            selectsql = f'select * from {tablename} limit 10'
+            # 组合前面的SQL
+            sqlsnippets = ',\n'.join([subquery.value for subquery in subqueries[:idx+1]])
+
+            combined_sql = SqlStatement.from_sqlsnippets(comment, sqlsnippets, selectsql)
+
+            combination_sqls.append(combined_sql)
+        return combination_sqls
+
+
+class SqlParse:
+
+    def __init__(self, sql: str):
+        self.sql = sql
+        self._statements = None
+
+    @property
+    def statements(self) -> list[SqlStatement, ]:
+        if self._statements is None:
+            sqls = [sql.strip() for sql in self.sql.split(';')]
+            self._statements = [SqlStatement(sql) for sql in sqls if sql]
+            if len(self._statements) > 1:
+                sqllogger.warning(f'SQL has {len(self._statements)} statements ~')
+        return self._statements
+
+    def get_statement(self, idx: int = 0):
+        statements = self.statements
+        stmt = statements[idx]
+        return stmt
